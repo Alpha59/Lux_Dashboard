cf844c40bd2c330a7fd63ea2a3e3200ea5e694b3
William <w.luong.266@gmail.com>
2015-10-21 23:29:42 -0400

    Moved bootstrap to an outside folder, off the project. Updated contact.js, but is now stuck with what 'Element' is.

--- Will Test/pages/js/contact.js
+++ Will Test/pages/js/contact.js
@@ -1,3 +1,32 @@
-window.onload = function() {
-	alert('contact!');
+function Contact(divId, options) {
+	
+	// Initial setup
+	this.placement = document.getElementById(divId);
+	this.placement.innerHTML = '';
+	
+	// Backend functions
+	this.server = {
+		adjust: function(data, callback) {
+			
+		},
+		query: function(data, callback) {
+			
+		},
+		sendmail: function(data, callback) {
+			
+		}
+	}
+	
+	// Replace/add functions on the backend when necessary
+	if (typeof options === 'object') {
+		for (var key in in options) {
+			if (options.hasOwnProperty(key) && Object.prototype.toString.call(options[key]) == '[object Function]')
+				this.server[key] = options[key];
+		}
+	}
+	
+	// Functions
+	this.addElement = function(name, options) {
+		
+	}
 }
